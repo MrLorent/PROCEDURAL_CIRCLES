0240b7ff6fce891d4c756cc7c6cbdf0569908e8f
T_Lorent <lorent.tanguy@gmail.com>
2021-03-27 15:50:38 +0100

    Upgrading_random_peak

--- build/build.js
+++ build/build.js
@@ -1,6 +1,7 @@
 var gui = new dat.GUI();
 var params = {
-    Random_Seed: 26,
+    Like_Original: 1,
+    Random_Seed: 108,
     Noise_Scale: 50,
     Noise_Frequence: 5,
     Bezier: 10,
@@ -9,7 +10,8 @@ var params = {
     Circles_Spacing: 15,
     Download_Image: function () { return save(); },
 };
-gui.add(params, "Random_Seed", 0, 100, 0.5);
+gui.add(params, "Like_Original", 0, 1, 1);
+gui.add(params, "Random_Seed", 0, 200, 0.5);
 gui.add(params, "Noise_Scale", 0, 100, 1);
 gui.add(params, "Noise_Frequence", 0, 20, 0.05);
 gui.add(params, "Bezier", 0, 50, 1);
@@ -18,17 +20,27 @@ gui.add(params, "lower_diameter", 0, 100, 1);
 gui.add(params, "Circles_Spacing", 0, 30, 0.5);
 gui.add(params, "Download_Image");
 function draw() {
-    randomSeed(params.Random_Seed);
+    randomSeed(params.Random_Seed * 100);
     background('white');
     translate(width / 2, height / 2);
     noFill();
     stroke('black');
     strokeWeight(3.5);
     var NB_CIRCLES = 15;
-    var picked = pickRandomly(NB_CIRCLES);
-    var HAS_PIC = picked[0];
-    var POLYGONE = picked[1];
-    var PERFECT_CIRCLE = picked[2];
+    var HAS_PIC;
+    var POLYGONE;
+    var PERFECT_CIRCLE;
+    if (params.Like_Original) {
+        HAS_PIC = 2;
+        POLYGONE = 8;
+        PERFECT_CIRCLE = 9;
+    }
+    else {
+        var picked = pickRandomly(NB_CIRCLES);
+        HAS_PIC = picked[0];
+        POLYGONE = picked[1];
+        PERFECT_CIRCLE = picked[2];
+    }
     for (var range = 0; range < NB_CIRCLES; range++) {
         var nbSubs = params.Circle_Subs + (range - 10);
         var circleAnchors = new Array();
@@ -52,13 +64,25 @@ function draw() {
             var angleSuivant = (i + 1) * (TWO_PI / nbSubs);
             var startControler = void 0;
             var endControler = void 0;
-            if (range == POLYGONE) {
-                startControler = circleAnchors[i];
-                endControler = circleAnchors[i + 1];
-            }
-            else {
-                startControler = generateControlMark(range, circleAnchors[i], angle, 1);
-                endControler = generateControlMark(range, circleAnchors[i + 1], angleSuivant, -1);
+            switch (range) {
+                case HAS_PIC:
+                    if (abs(circleAnchors[i + 1].y - circleAnchors[i].y) > 50) {
+                        startControler = generateShortControlMark(range, circleAnchors[i], angle, 1);
+                        endControler = generateShortControlMark(range, circleAnchors[i + 1], angleSuivant, -1);
+                    }
+                    else {
+                        startControler = generateControlMark(range, circleAnchors[i], angle, 1);
+                        endControler = generateControlMark(range, circleAnchors[i + 1], angleSuivant, -1);
+                    }
+                    break;
+                case POLYGONE:
+                    startControler = circleAnchors[i];
+                    endControler = circleAnchors[i + 1];
+                    break;
+                default:
+                    startControler = generateControlMark(range, circleAnchors[i], angle, 1);
+                    endControler = generateControlMark(range, circleAnchors[i + 1], angleSuivant, -1);
+                    break;
             }
             bezier(circleAnchors[i].x, circleAnchors[i].y, startControler.x, startControler.y, endControler.x, endControler.y, circleAnchors[i + 1].x, circleAnchors[i + 1].y);
         }
@@ -99,7 +123,7 @@ function draw() {
     }
     function generatePic(nbSubs, anchors) {
         var pic = int(random(0, nbSubs));
-        anchors[pic].y = anchors[pic].y - (height / 2 + anchors[pic].y - 60);
+        anchors[pic].y = anchors[pic].y - (height / 2 + anchors[pic].y - random(50, height / 2 + anchors[pic].y + 55));
     }
     function generatePerfectCircle(nbSubs, circleNumber, anchors) {
         for (var i = 0; i < nbSubs; i++) {
@@ -114,31 +138,31 @@ function draw() {
     }
     function generateControlMark(circleNumber, anchor, angle, orientation) {
         var x, y;
-        if (anchor.y == 60 - height / 2) {
-            if (sin(angle) < 0) {
-                x = anchor.x + orientation * 5;
-                y = anchor.y;
+        if (circleNumber >= 0 && circleNumber < 3 && circleNumber != PERFECT_CIRCLE) {
+            if ((cos(angle) > cos(PI / 4) || cos(angle) < cos(3 * PI / 4))) {
+                x = anchor.x;
+                y = anchor.y + cos(angle) / abs(cos(angle)) * orientation * params.Bezier;
             }
             else {
-                x = anchor.x - orientation * 5;
+                x = anchor.x - sin(angle) / abs(sin(angle)) * orientation * params.Bezier;
                 y = anchor.y;
             }
         }
         else {
-            if (circleNumber >= 0 && circleNumber < 3 && circleNumber != PERFECT_CIRCLE) {
-                if ((cos(angle) > cos(PI / 4) || cos(angle) < cos(3 * PI / 4))) {
-                    x = anchor.x;
-                    y = anchor.y + cos(angle) / abs(cos(angle)) * orientation * params.Bezier;
-                }
-                else {
-                    x = anchor.x - sin(angle) / abs(sin(angle)) * orientation * params.Bezier;
-                    y = anchor.y;
-                }
-            }
-            else {
-                x = sqrt((pow(anchor['x'], 2) + pow(anchor['y'], 2)) + pow(params.Bezier, 2)) * cos(angle + orientation * (PI / 2 - atan(sqrt(pow(anchor['x'], 2) + pow(anchor['y'], 2)) / (params.Bezier + circleNumber / 2))));
-                y = sqrt((pow(anchor['x'], 2) + pow(anchor['y'], 2)) + pow(params.Bezier, 2)) * sin(angle + orientation * (PI / 2 - atan(sqrt(pow(anchor['x'], 2) + pow(anchor['y'], 2)) / (params.Bezier + circleNumber / 2))));
-            }
+            x = sqrt((pow(anchor['x'], 2) + pow(anchor['y'], 2)) + pow(params.Bezier, 2)) * cos(angle + orientation * (PI / 2 - atan(sqrt(pow(anchor['x'], 2) + pow(anchor['y'], 2)) / (params.Bezier + circleNumber / 2))));
+            y = sqrt((pow(anchor['x'], 2) + pow(anchor['y'], 2)) + pow(params.Bezier, 2)) * sin(angle + orientation * (PI / 2 - atan(sqrt(pow(anchor['x'], 2) + pow(anchor['y'], 2)) / (params.Bezier + circleNumber / 2))));
+        }
+        return { 'x': x, 'y': y };
+    }
+    function generateShortControlMark(circleNumber, anchor, angle, orientation) {
+        var x, y;
+        if (sin(angle) < 0) {
+            x = anchor.x + orientation * (5 + HAS_PIC / 3);
+            y = anchor.y;
+        }
+        else {
+            x = anchor.x - orientation * (5 + HAS_PIC / 3);
+            y = anchor.y;
         }
         return { 'x': x, 'y': y };
     }
--- src/sketch.ts
+++ src/sketch.ts
@@ -4,7 +4,8 @@
 
 const gui = new dat.GUI()
 const params = {
-    Random_Seed: 26,
+    Like_Original: 1,
+    Random_Seed: 108,
     Noise_Scale: 50,
     Noise_Frequence: 5,
     Bezier: 10,
@@ -13,7 +14,8 @@ const params = {
     Circles_Spacing: 15,
     Download_Image: () => save(),
 }
-gui.add(params, "Random_Seed", 0, 100, 0.5);
+gui.add(params, "Like_Original", 0, 1, 1);
+gui.add(params, "Random_Seed", 0, 200, 0.5);
 gui.add(params, "Noise_Scale", 0, 100, 1);
 gui.add(params, "Noise_Frequence", 0, 20, 0.05);
 gui.add(params, "Bezier", 0, 50, 1);
@@ -27,7 +29,7 @@ gui.add(params, "Download_Image");
 // -------------------
 
 function draw() {
-    randomSeed(params.Random_Seed);
+    randomSeed(params.Random_Seed * 100);
     background('white');
     translate(width/2, height/2);
 
@@ -36,16 +38,21 @@ function draw() {
     strokeWeight(3.5);
 
     //  INITIALIZATION
-    // const NB_CIRCLES = 15;
-    // const HAS_PIC = 2;
-    // const POLYGONE = 8;
-    // const PERFECT_CIRCLE = 9;
+    var NB_CIRCLES = 15;
+    var HAS_PIC;
+    var POLYGONE;
+    var PERFECT_CIRCLE;
 
-    const NB_CIRCLES = 15;
-    var picked = pickRandomly(NB_CIRCLES);
-    const HAS_PIC = picked[0];
-    const POLYGONE = picked[1];
-    const PERFECT_CIRCLE = picked[2];
+    if(params.Like_Original){
+        HAS_PIC = 2;
+        POLYGONE = 8;
+        PERFECT_CIRCLE = 9;
+    }else{
+        var picked = pickRandomly(NB_CIRCLES);
+        HAS_PIC = picked[0];
+        POLYGONE = picked[1];
+        PERFECT_CIRCLE = picked[2];
+    }
 
     // DRAWING OF THE FIFTEEN CIRCLES
     for(let range=0; range<NB_CIRCLES; range++){
@@ -81,12 +88,25 @@ function draw() {
             let startControler;
             let endControler;
 
-            if(range == POLYGONE){
-                startControler = circleAnchors[i];
-                endControler = circleAnchors[i+1];
-            }else{
-                startControler = generateControlMark(range, circleAnchors[i], angle, 1);
-                endControler = generateControlMark(range, circleAnchors[i+1], angleSuivant, -1);
+            switch(range){
+                case HAS_PIC:
+                    if(abs(circleAnchors[i+1].y - circleAnchors[i].y) > 50){
+                        startControler = generateShortControlMark(range, circleAnchors[i], angle, 1);
+                        endControler = generateShortControlMark(range, circleAnchors[i+1], angleSuivant, -1);
+                    }else{
+                        startControler = generateControlMark(range, circleAnchors[i], angle, 1);
+                        endControler = generateControlMark(range, circleAnchors[i+1], angleSuivant, -1);
+                    
+                    }
+                    break;
+                case POLYGONE:
+                    startControler = circleAnchors[i];
+                    endControler = circleAnchors[i+1];
+                    break;
+                default:
+                    startControler = generateControlMark(range, circleAnchors[i], angle, 1);
+                    endControler = generateControlMark(range, circleAnchors[i+1], angleSuivant, -1);
+                    break;
             }
 
             bezier(
@@ -148,7 +168,7 @@ function draw() {
 
     function generatePic(nbSubs, anchors){
         let pic = int(random(0, nbSubs));
-        anchors[pic].y = anchors[pic].y - (height/2 + anchors[pic].y - 60);
+        anchors[pic].y = anchors[pic].y - (height/2 + anchors[pic].y - random(50, height/2 + anchors[pic].y + 55));
     }
 
     function generatePerfectCircle(nbSubs, circleNumber, anchors){
@@ -168,30 +188,32 @@ function draw() {
 
     function generateControlMark(circleNumber, anchor, angle, orientation){
         let x, y;
-        if(anchor.y == 60 - height/2){
-            if(sin(angle) < 0){
-                x = anchor.x + orientation*5;
-                y = anchor.y;
+        if(circleNumber >= 0 && circleNumber <3 && circleNumber != PERFECT_CIRCLE){
+            if((cos(angle) > cos(PI/4) || cos(angle) < cos(3*PI/4))){
+                x = anchor.x;
+                y = anchor.y + cos(angle)/abs(cos(angle)) * orientation * params.Bezier;
             }else{
-                x = anchor.x - orientation*5;
+                x = anchor.x - sin(angle)/abs(sin(angle)) * orientation * params.Bezier;
                 y = anchor.y;
             }
         }else{
-            if(circleNumber >= 0 && circleNumber <3 && circleNumber != PERFECT_CIRCLE){
-                if((cos(angle) > cos(PI/4) || cos(angle) < cos(3*PI/4))){
-                    x = anchor.x;
-                    y = anchor.y + cos(angle)/abs(cos(angle)) * orientation * params.Bezier;
-                }else{
-                    x = anchor.x - sin(angle)/abs(sin(angle)) * orientation * params.Bezier;
-                    y = anchor.y;
-                }
-            }else{
-                x = sqrt((pow(anchor['x'],2)+pow(anchor['y'],2)) + pow(params.Bezier, 2)) * cos(angle + orientation * (PI/2 - atan(sqrt(pow(anchor['x'],2)+pow(anchor['y'],2))/(params.Bezier + circleNumber/2))));
+            x = sqrt((pow(anchor['x'],2)+pow(anchor['y'],2)) + pow(params.Bezier, 2)) * cos(angle + orientation * (PI/2 - atan(sqrt(pow(anchor['x'],2)+pow(anchor['y'],2))/(params.Bezier + circleNumber/2))));
 
-                y = sqrt((pow(anchor['x'],2)+pow(anchor['y'],2)) + pow(params.Bezier, 2)) * sin(angle + orientation * (PI/2 - atan(sqrt(pow(anchor['x'],2)+pow(anchor['y'],2))/(params.Bezier + circleNumber/2))));
-            }
+            y = sqrt((pow(anchor['x'],2)+pow(anchor['y'],2)) + pow(params.Bezier, 2)) * sin(angle + orientation * (PI/2 - atan(sqrt(pow(anchor['x'],2)+pow(anchor['y'],2))/(params.Bezier + circleNumber/2))));
         }
 
+        return {'x':x, 'y':y};
+    }
+
+    function generateShortControlMark(circleNumber, anchor, angle, orientation){
+        let x, y;
+        if(sin(angle) < 0){
+            x = anchor.x + orientation*(5 + HAS_PIC/3);
+            y = anchor.y;
+        }else{
+            x = anchor.x - orientation*(5 + HAS_PIC/3);
+            y = anchor.y;
+        }
         return {'x':x, 'y':y};
     }
     
